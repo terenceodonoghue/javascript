315f3f8401468e377e45644b32f22e43e119204f
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-03-28 20:21:58 +1100

    Create generator-js package

---
+++ packages/generator-js/src/generators/app/index.ts
@@ -0,0 +1,78 @@
+import path = require('path');
+import Generator = require('yeoman-generator');
+
+interface Answers {
+  name: string;
+  workspace: string;
+}
+
+module.exports = class extends Generator {
+  private answers?: Answers;
+
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What's the project called?",
+        default: path.basename(process.cwd()),
+      },
+      {
+        type: 'confirm',
+        name: 'workspace',
+        message: 'Is it a workspace?',
+        default: false,
+      },
+    ]);
+  }
+
+  configuring() {
+    this.fs.copyTpl(
+      this.templatePath('**/*'),
+      this.destinationPath(),
+      {
+        name: this.answers?.name,
+        year: new Date().getFullYear(),
+      },
+      undefined,
+      { globOptions: { dot: true } },
+    );
+  }
+
+  install() {
+    this.spawnCommandSync('yarn', ['set', 'version', 'berry']);
+
+    this.spawnCommandSync('yarn', [
+      'init',
+      this.answers?.workspace ? '-w' : '-y',
+    ]);
+
+    this.spawnCommandSync('yarn', [
+      'config',
+      'set',
+      'initScope',
+      'terenceodonoghue',
+    ]);
+
+    this.spawnCommandSync('yarn', [
+      'config',
+      'set',
+      'nodeLinker',
+      'node-modules',
+    ]);
+
+    this.spawnCommandSync('yarn', [
+      'config',
+      'set',
+      'npmScopes.terenceodonoghue.npmAuthToken',
+      `\${GITHUB_TOKEN}`,
+    ]);
+
+    this.spawnCommandSync('yarn', [
+      'config',
+      'set',
+      'npmScopes.terenceodonoghue.npmRegistryServer',
+      'https://npm.pkg.github.com',
+    ]);
+  }
+};
