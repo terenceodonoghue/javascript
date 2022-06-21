a107fa179789056e2d9e98820cd0815a95335a73
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-06-21 11:13:41 +1000

    Ignore core and relative modules in import resolver

--- packages/eslint-import-resolver-exports/index.js
+++ packages/eslint-import-resolver-exports/index.js
@@ -1,32 +1,40 @@
 const resolve = require('resolve');
-const getPackageName = require('./utils/getPackageName');
+
+const id = (source) => {
+  if (source.startsWith('@')) {
+    const [scope, package] = source.split('/');
+    return [scope, package].join('/');
+  }
+
+  const [package] = source.split('/');
+  return package;
+};
 
 exports.interfaceVersion = 2;
 
 exports.resolve = (source) => {
+  if (resolve.isCore(source) || source.startsWith('.')) {
+    return { found: false };
+  }
+
   try {
-    const resolvedPath = resolve.isCore(source)
-      ? null
-      : resolve.sync(getPackageName(source), {
-          packageFilter: (pkg) => {
-            const packageJson = pkg;
+    const resolvedPath = resolve.sync(id(source), {
+      packageFilter: (pkg) => {
+        const packageJson = pkg;
 
-            if (packageJson.exports) {
-              const path = source
-                .split(packageJson.name)
-                .map((subpath) => `.${subpath}`)
-                .pop();
+        if (packageJson.exports) {
+          const subpath = `.${source.split(packageJson.name).pop()}`;
 
-              if (packageJson.exports[path]) {
-                packageJson.main =
-                  packageJson.exports[path].default ||
-                  packageJson.exports[path];
-              }
-            }
+          if (packageJson.exports[subpath]) {
+            packageJson.main =
+              packageJson.exports[subpath].default ||
+              packageJson.exports[subpath];
+          }
+        }
 
-            return packageJson;
-          },
-        });
+        return packageJson;
+      },
+    });
 
     return { found: true, path: resolvedPath };
   } catch (error) {
