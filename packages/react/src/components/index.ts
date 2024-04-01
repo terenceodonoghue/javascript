9b93d703d5dbcb7308735f0f4d06be41f6ad83ab
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2024-04-01 16:27:39 +1100

    Create react package

---
+++ packages/react/src/components/index.ts
@@ -0,0 +1,4 @@
+export { ThemeProvider } from '@emotion/react';
+
+export { default as Flex, type FlexProps } from './primitives/Flex.js';
+export { default as Grid, type GridProps } from './primitives/Grid.js';
---
+++ packages/react/src/components/primitives/Flex.tsx
@@ -0,0 +1,43 @@
+/** @jsxImportSource @emotion/react */
+import { ElementType } from 'react';
+
+import { Container, Primitive, Themeable } from '../types.js';
+
+export interface FlexProps extends Container, Themeable {
+  /**
+   * Sets the gap (in rem) between rows and columns
+   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/gap
+   */
+  gx?: number;
+}
+
+const Flex = <T extends ElementType = 'div'>({
+  as,
+  gx = 1,
+  mx = 0,
+  my = 0,
+  px = 0,
+  py = 0,
+  sx,
+  ...props
+}: Primitive<T, FlexProps>) => {
+  const Element = as || 'div';
+  return (
+    <Element
+      css={[
+        {
+          display: 'flex',
+          gap: `${gx}rem`,
+          marginInline: `${mx}rem`,
+          marginBlock: `${my}rem`,
+          paddingInline: `${px}rem`,
+          paddingBlock: `${py}rem`,
+        },
+        sx,
+      ]}
+      {...props}
+    />
+  );
+};
+
+export default Flex;
