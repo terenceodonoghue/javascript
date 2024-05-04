23e0561535d65d45fcf287aafe70cae9a2049029
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2024-05-04 22:16:45 +1000

    Update lint configs

--- packages/eslint-plugin-javascript/configs/react.js
+++ packages/eslint-plugin-javascript/configs/react.js
@@ -9,9 +9,13 @@ const react = compat.config({
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
     'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
   settings: {
     react: {
