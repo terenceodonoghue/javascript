241d7deab459ff12de5d7c34418cb38eec896cd3
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2025-05-19 09:10:08 +1000

    Fix types

--- packages/react/src/components/emotion.d.ts
+++ packages/react/src/components/emotion.d.ts
@@ -1,6 +1,6 @@
 import '@emotion/react';
 
-import { Theme as CSSTheme } from '@terenceodonoghue/css';
+import { Theme as CSSTheme } from '@terenceodonoghue/css/themes';
 
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
